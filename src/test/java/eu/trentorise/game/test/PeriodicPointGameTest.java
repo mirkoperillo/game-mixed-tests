62eb0e92d04191da533325b82e4b350176f3a0f2
mirkoperillo <mirko.perillo@gmail.com>
2017-07-18 11:51:40 +0200

    another sample

---
+++ src/test/java/eu/trentorise/game/test/PeriodicPointGameTest.java
@@ -0,0 +1,69 @@
+package eu.trentorise.game.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTimeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import eu.trentorise.game.model.PointConcept;
+import eu.trentorise.game.model.core.GameConcept;
+import eu.trentorise.game.services.PlayerService;
+
+public class PeriodicPointGameTest extends GameTest {
+
+	@Autowired
+	PlayerService playerSrv;
+
+	private static final String GAME = "periodicScore";
+	private static final String ACTION = "save_itinerary";
+
+	@Override
+	public void initEnv() {
+
+	}
+
+	@Override
+	public void defineGame() {
+		List<GameConcept> concepts = new ArrayList<GameConcept>();
+		PointConcept p = new PointConcept("green leaves");
+
+		Date startDate = new Date(); // today
+		long dayDurationInMillis = 24 * 60 * 60 * 1000;
+		p.addPeriod("weekly", startDate, dayDurationInMillis);
+		concepts.add(p);
+
+		defineGameHelper(GAME, Arrays.asList(ACTION), concepts);
+
+		loadClasspathRules(GAME,
+				Arrays.asList("rules/" + GAME + "/constants", "rules/" + GAME + "/greenPoints.drl",
+						"rules/" + GAME + "/greenBadges.drl", "rules/" + GAME + "/specialBadges.drl",
+						"rules/" + GAME + "/weekClassificationBadges.drl"));
+	}
+
+	@Override
+	public void defineExecData(List<ExecData> execList) {
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("walkDistance", 1d);
+		ExecData input = new ExecData(GAME, ACTION, "prowler", data);
+		execList.add(input);
+
+		DateTimeUtils.setCurrentMillisFixed(new GregorianCalendar(2016, 3, 26, 3, 22).getTimeInMillis());
+		data = new HashMap<String, Object>();
+		data.put("bikeDistance", 5d);
+		input = new ExecData(GAME, ACTION, "prowler", data);
+		execList.add(input);
+
+	}
+
+	@Override
+	public void analyzeResult() {
+
+	}
+
+}
