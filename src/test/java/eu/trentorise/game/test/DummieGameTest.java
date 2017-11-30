72c07f4f8cb330ee6c134fd94548ff2d09616fab
mirkoperillo <mirko.perillo@gmail.com>
2017-11-30 14:05:04 +0100

    added test to show a code snippet

---
+++ src/test/java/eu/trentorise/game/test/DummieGameTest.java
@@ -0,0 +1,70 @@
+package eu.trentorise.game.test;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import eu.trentorise.game.model.PointConcept;
+import eu.trentorise.game.model.PointConceptStateHelperFactory;
+import eu.trentorise.game.model.PointConceptStateHelperFactory.PointConceptStateHelper;
+import eu.trentorise.game.model.core.GameConcept;
+
+public class DummieGameTest extends GameTest {
+
+    @Autowired
+    private PointConceptStateHelperFactory helperFactory;
+
+    private static final String GAME = "periodicScore";
+    private static final String ACTION = "save_itinerary";
+
+
+    @Override
+    public void initEnv() {
+        Date moment = Date.from(
+                LocalDate.now().minusDays(5).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        PointConceptStateHelper h =
+                helperFactory.instanceOf(GAME, "green leaves").setScoreInTime(moment, 5d);
+        moment = Date.from(
+                LocalDate.now().minusDays(3).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        h.setScoreInTime(moment, 12d);
+        moment = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        h.setScoreInTime(moment, 13d);
+        savePlayerState(GAME, "gambit", Arrays.asList(h.build()));
+
+
+    }
+
+    @Override
+    public void defineGame() {
+        List<GameConcept> concepts = new ArrayList<GameConcept>();
+        PointConcept p = new PointConcept("green leaves");
+
+        Date startDate = Date.from(
+                LocalDate.now().minusDays(7).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        long dayDurationInMillis = 24 * 60 * 60 * 1000;
+        p.addPeriod("weekly", startDate, dayDurationInMillis);
+        concepts.add(p);
+
+        defineGameHelper(GAME, Arrays.asList(ACTION), concepts);
+
+    }
+
+    @Override
+    public void defineExecData(List<ExecData> execList) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void analyzeResult() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
